5cf93a519a6dc24ec8e78e2700d559a3a4148a4a
DrewLive <andrew.rocha06@gmail.com>
2025-05-23 12:28:43 -0600

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 
 playlist_urls = [
-    "https://tinyurl.com/daddylive08",
+    "https://tinyurl.com/daddylive-24",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DrewAll.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/TheTVApp.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/JapanTV.m3u8",
