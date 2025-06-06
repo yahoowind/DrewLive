2765c0dc66bd1d7d11cbd46a58779b7a609b80b3
DrewLive <andrew.rocha06@gmail.com>
2025-06-06 14:02:56 -0600

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -17,6 +17,7 @@ playlist_urls = [
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DrewLiveVOD.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DaddyLiveEvents.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/UDPTV.m3u",
+    "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/Drew247TV.m3u8",
 ]
 
 EPG_URL = "https://tinyurl.com/merged2423-epg"
