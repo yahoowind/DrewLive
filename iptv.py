045995176f3a9af97ca0caf1cd2a8ad80f753bff
DrewLive <andrew.rocha06@gmail.com>
2025-06-10 07:03:04 -0600

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -23,7 +23,8 @@ playlist_urls = [
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/RoxieStreams.m3u",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/Tims247Live.m3u",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/PixelSports.m3u",
-    "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/FSTVLive.m3u"
+    "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/FSTVLive.m3u",
+    "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/Radio.m3u",
 ]
 
 UDPTV_URL = "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/UDPTV.m3u"
