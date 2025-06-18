7a3089e2319d58d36c3a2f62fd244c9f430917f4
DrewLive <andrew.rocha06@gmail.com>
2025-06-17 19:30:24 -0600

    Update update_daddylive.py

--- update_daddylive.py
+++ update_daddylive.py
@@ -16,7 +16,13 @@ LOCKED_CHANNELS = {
     "Discovery Channel": "https://hipaf6u2j3pwygg.nice-flower.store/v4/variant/VE1gTdz0mLzRnLv52bt9SMhFjdtM3ajFmc09SMkVDZldTMjRmM1UWL4gzY40yYyYDNtM2YhFTL4QTYxcDMwM2L.m3u8?md5=P_ZRKSUAQTEs1klzlorEJQ&expires=1750248906&t=1750205706",
     "Discovery Life Channel": "https://hipaf6u2j3pwygg.nice-flower.store/v3/director/VE1ODhlZDkzOTA2MzJhLTQ5YmEtZjg1NC04NTU3LTk2NzEwMTI1/master.m3u8?md5=XktZkv3c_ytZI_JZBEzzkg&expires=1750250162&t=1750206962",
     "Disney Channel": "https://hipaf6u2j3pwygg.nice-flower.store/v3/director/VE1NWExNTMwNzQ3NzMyLTdkYjgtMGQ5NC0zNDkwLWI4YThkYzk1/master.m3u8?md5=KO3sVCpU1airg_ymD2eVVg&expires=1750250481&t=1750207281",
-
+    "Disney XD": "https://hipaf6u2j3pwygg.nice-flower.store/v3/director/VE1NWNmYmE3M2Q4Y2UxLTEwYjktN2IxNC01ZGE1LTY4ZmE1NTU5/master.m3u8?md5=E65aRLZRBc2eJbPhoWOaxQ&expires=1750251145&t=1750207945",
+    "E! Entertainment": "https://hipaf6u2j3pwygg.nice-flower.store/v3/director/VE1NWNmYmE3M2Q4Y2UxLTEwYjktN2IxNC01ZGE1LTY4ZmE1NTU5/master.m3u8?md5=E65aRLZRBc2eJbPhoWOaxQ&expires=1750251145&t=1750207945",
+    "ESPN Deportes": "https://hipaf6u2j3pwygg.nice-flower.store/v3/director/VE1MzE5ZWM0NGYxNWExLTEzOTktMTllNC03MDAxLWQ1ZGM1YzFm/master.m3u8?md5=45wiyaAEGRzKX9xH-kqlWw&expires=1750251311&t=1750208111",
+    "ESPN USA": "https://hipaf6u2j3pwygg.nice-flower.store/v3/director/VE1Yzc1NzQ2MmVkOGViLTUyYTgtNTc2NC0xNzVjLWE2MmQyOGE4/master.m3u8?md5=hDThTRzjr2pxCz77uL3lAA&expires=1750251420&t=1750208220",
+    "ESPN2 USA": "https://hipaf6u2j3pwygg.nice-flower.store/v3/director/VE1NDJlMzQ4NzNiMGYzLTA2OTgtYWQ1NC1lN2VmLTlkZWQ3YTQy/master.m3u8?md5=FAr8MitrGdCukq9CJcurUg&expires=1750251703&t=1750208503",
+    "ESPNews": "https://hipaf6u2j3pwygg.nice-flower.store/v3/director/VE1N2U1ODA1MmJmNDA5LTVhZjktNDI1NC0xNmJmLTA0ZTVkZGE5/master.m3u8?md5=jKgUIMiX8G-4MTA1YaxdgQ&expires=1750251749&t=1750208549",
+    "ESPNU USA": "https://hipaf6u2j3pwygg.nice-flower.store/v3/director/VE1NDJlY2IxMDVhM2ZmLWUzMDgtMWI4NC00OTVmLTQzM2YyZjY0/master.m3u8?md5=N3WhiOsCfrX",
 }
 
 def update_playlist():
