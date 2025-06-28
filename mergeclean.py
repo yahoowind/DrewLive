df4b39eb55d82cbbf3dc63c78e7cc4d25cfcf2df
DrewLive <andrew.rocha06@gmail.com>
2025-06-28 15:22:22 -0600

    Update mergeclean.py

--- mergeclean.py
+++ mergeclean.py
@@ -19,6 +19,7 @@ playlist_urls = [
     "http://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DaddyLiveEvents.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/PPVLand.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/StreamEast.m3u8",
+    "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/FSTV24.m3u8",
 ]
 
 UDPTV_URL = "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/UDPTV.m3u"
