181f807e69045c737aae3ac85169b355de221ba6
DrewLive <andrew.rocha06@gmail.com>
2025-08-25 08:54:13 -0600

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 playlist_urls = [
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DaddyLive.m3u8",
+    "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DaddyLiveEvents.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DrewAll.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/JapanTV.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/PlexTV.m3u8",
