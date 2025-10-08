2f702a96c4ef151c8037f8042ee532a07aa99ee8
DrewLive <andrew.rocha06@gmail.com>
2025-10-07 22:14:50 -0600

    Update mergeclean.py

--- mergeclean.py
+++ mergeclean.py
@@ -25,7 +25,8 @@ playlist_urls = [
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/SportsWebcast.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/TubiTV.m3u8",
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/Roku.m3u8",
-    "http://drewlive24.duckdns.org/daddylive/stream.php"
+    "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DaddyLive.m3u8",
+    "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DaddyLive.m3u8"
 ]
 
 EPG_URL = "http://drewlive24.duckdns.org:8081/DrewLive.xml.gz"
